ec1874a44ff299308ef271125afa39422bcb221c
takabayap <takabayap@gmail.com>
2023-06-23 14:27:33 +0900

    update: add rendering

--- mutex_visualiser.go
+++ mutex_visualiser.go
@@ -3,10 +3,15 @@ package mutexvisualiser
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"runtime"
+	"sort"
 	"strconv"
 	"sync"
+	"text/template"
 	"time"
+
+	"github.com/goccy/go-graphviz"
 )
 
 type Mutex[T any] interface {
@@ -40,7 +45,7 @@ const (
 )
 
 func (a actionType) String() string {
-	return []string{"lock_start", "lock_end", "unlock", "set", "read"}[a]
+	return []string{"lock_start", "lock", "unlock", "set", "read"}[a]
 }
 
 func (m *MutexVisualiser[T]) PrintAll() {
@@ -80,6 +85,110 @@ func (m *MutexVisualiser[T]) Set(t T) {
 		actionTime: time.Now(),
 	})
 	m.value = t
+}
+
+func (m *MutexVisualiser[T]) RenderGraph(path string) {
+	sort.Slice(m.actions, func(i, j int) bool {
+		return m.actions[i].actionTime.Before(m.actions[j].actionTime)
+	})
+
+	g := graphviz.New()
+	var b []byte
+	var graphStr string
+
+	t := template.New("dot")
+	t.Parse(tmplGraph)
+
+	var tmp bytes.Buffer
+	var GNos map[uint64]int = make(map[uint64]int)
+	GNos[1] = 1
+
+	tNo := 1
+	mNo := 1
+	for _, v := range m.actions {
+		if _, ok := GNos[v.gID]; !ok {
+			GNos[v.gID] = 1
+			graphStr += createGBranch(v.gID, v.parentGID)
+		}
+		switch v.actionType {
+		case lockStart:
+			graphStr += actionOnG("waiting for lock...", v.gID, GNos[v.gID], tNo)
+		case read:
+			graphStr += actionToM("read", "back", v.gID, GNos[v.gID], tNo, mNo)
+			mNo++
+		case unlock:
+			graphStr += actionToM("unlock", "back", v.gID, GNos[v.gID], tNo, mNo)
+			mNo++
+		default:
+			graphStr += actionToM(v.actionType.String(), "forward", v.gID, GNos[v.gID], tNo, mNo)
+			mNo++
+		}
+		tNo++
+		GNos[v.gID]++
+	}
+	if err := t.Execute(&tmp, graphStr); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(tmp.String())
+	b = append(b, tmp.Bytes()...)
+
+	graph, err := graphviz.ParseBytes(b)
+	if err != nil {
+		fmt.Println("Error parsing graphviz bytes", err)
+	}
+	if err := g.RenderFilename(graph, graphviz.SVG, path); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createGBranch(GID uint64, PGID uint64) string {
+	t := template.New("branch")
+	t.Parse(tmplCreateGBranch)
+	var tmp bytes.Buffer
+	if err := t.Execute(&tmp, map[string]uint64{
+		"GID":  GID,
+		"PGID": PGID,
+	}); err != nil {
+		log.Fatal(err)
+	}
+	return tmp.String()
+}
+
+func actionToM(ActionType string, ActionDir string, GID uint64, GNo int, TNo int, MNo int) string {
+	t := template.New("set")
+	t.Parse(tmplActionToMutex)
+	var tmp bytes.Buffer
+	if err := t.Execute(&tmp, map[string]string{
+		"ActionType": ActionType,
+		"ActionDir":  ActionDir,
+		"GID":        fmt.Sprint(GID),
+		"GNo":        strconv.Itoa(GNo),
+		"NextGNo":    strconv.Itoa(GNo + 1),
+		"TNo":        strconv.Itoa(TNo),
+		"NextTNo":    strconv.Itoa(TNo + 1),
+		"MNo":        strconv.Itoa(MNo),
+		"NextMNo":    strconv.Itoa(MNo + 1),
+	}); err != nil {
+		log.Fatal(err)
+	}
+	return tmp.String()
+}
+
+func actionOnG(ActionType string, GID uint64, GNo int, TNo int) string {
+	t := template.New("on")
+	t.Parse(tmplActionOnG)
+	var tmp bytes.Buffer
+	if err := t.Execute(&tmp, map[string]string{
+		"ActionType": ActionType,
+		"GID":        fmt.Sprint(GID),
+		"GNo":        strconv.Itoa(GNo),
+		"NextGNo":    strconv.Itoa(GNo + 1),
+		"TNo":        strconv.Itoa(TNo),
+		"NextTNo":    strconv.Itoa(TNo + 1),
+	}); err != nil {
+		log.Fatal(err)
+	}
+	return tmp.String()
 }
 
 const timeFormat = "15:04:05.000000"
---
+++ templates.go
@@ -0,0 +1,73 @@
+package mutexvisualiser
+
+const tmplGraph = `
+digraph g{
+rankdir="TB";
+nodesep=0.5;
+ranksep=0.25;
+splines=line;
+forcelabels=false;
+
+// general
+node [style=filled, color="black", 
+    fontcolor="black", font="Consolas", fontsize="8pt" ];
+edge [arrowhead=vee, color="black", penwidth=2,dir=none, fontsize="6pt"];
+
+// graph
+node [width=0.2, height=0.2,  label="", margin="0.11,0.055", shape=circle, penwidth=2, fillcolor="#FF8000"]
+
+// timeline
+t1[group="time", label="time",shape="square"];
+
+// mutex
+m1[group="mutex",label="mutex", shape="square"];
+
+// main
+g1_1[group="g1",label="main", shape="square"];
+
+subgraph{
+    rank="same"
+    g1_1;
+    m1;
+}
+
+{{.}}
+}
+`
+const tmplCreateGBranch = `
+g{{.GID}}_1[group="g{{ .GID }}", label="{{ .GID }}", shape="square"];
+g{{.PGID}}_1->g{{.GID}}_1;
+subgraph{
+	rank="same"
+	g{{.GID}}_1;
+	m1;
+}
+`
+
+const tmplActionToMutex = `
+t{{.NextTNo}}[group="time"];
+t{{.TNo}}->t{{.NextTNo}};
+g{{.GID}}_{{.NextGNo}}[group="g{{.GID}}"];
+g{{.GID}}_{{.GNo}}->g{{.GID}}_{{.NextGNo}};
+m{{.NextMNo}} [group="mutex"];
+m{{.MNo}}->m{{.NextMNo}};
+subgraph{
+    rank="same"
+    m{{.NextMNo}};
+    t{{.NextTNo}};
+    g{{.GID}}_{{.NextGNo}};
+}
+g{{.GID}}_{{.NextGNo}}->m{{.NextMNo}}[label="{{.ActionType}}" dir="{{.ActionDir}}"];
+`
+
+const tmplActionOnG = `
+t{{.NextTNo}}[group="time"];
+t{{.TNo}}->t{{.NextTNo}};
+g{{.GID}}_{{.NextGNo}}[group="g{{.GID}}"];
+g{{.GID}}_{{.GNo}}->g{{.GID}}_{{.NextGNo}}[label="{{.ActionType}}" dir="forward"];	
+subgraph{
+    rank="same"
+    t{{.NextTNo}};
+    g{{.GID}}_{{.NextGNo}};
+}
+`
